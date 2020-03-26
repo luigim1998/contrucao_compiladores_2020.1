4837f72c8fad0424afb8d9ae693d140d4d6cf189
luigim1998 <sluigimuller@gmail.com>
2020-03-26 00:19:43 -0400

    Whitespace separation method

--- lexical_analyser.py
+++ lexical_analyser.py
@@ -1,6 +1,7 @@
 import re
+import json
 
-class lexical_analyser:
+class LexicalAnalyser:
     def __init__(self):
         with open("patterns.json") as js_file:
             data = json.load(js_file)
@@ -10,7 +11,8 @@ class lexical_analyser:
         self.INTEGER = data['regex']['INTEGER']
         self.FLOAT = data['regex']['FLOAT']
         self.STRING = data['regex']['STRING']
-        self.delimit = data['delimit']
+        self.DELIMIT = data['regex']['DELIMIT']
+        self.table = []
     
     def isKeyword(self, tok):
         return tok in self.keywords
@@ -58,4 +60,53 @@ class lexical_analyser:
         return False
     
     def isDelimit(self, tok):
-        return tok in self.delimit
+        r = re.compile(self.DELIMIT)
+        m = r.match(tok)
+        if(m == None):
+            return False
+        if(m.end() - m.start() == len(tok)):
+            return True
+        return False
+
+    def separate_delimits(self, text):
+        m = re.compile(self.DELIMIT)
+        delimiters = list(m.finditer(text))
+        cont = 0
+
+        for i in delimiters:
+            if(cont != i.start()): #existe algo antes do delimitador
+                self.table.append(text[cont:i.start()])
+            self.table.append(text[i.start()]) #adiciona o delimitador
+            cont = i.start()+1
+        
+        if(cont != len(text)): #existe texto após o último delimitador
+            self.table.append(text[cont:])
+
+    def separate_whitespace(self):
+        cont = 0
+        p = re.compile("[\S]+")
+        while(cont < len(self.table)):
+            delimiters = list(p.finditer(self.table[cont]))
+            if(len(delimiters) == 0): # não há texto
+                self.table.pop(cont)
+                cont += 1
+                # print("Cont: {}, Texto: {}, lista: {}".format(cont, self.table[cont], self.table))
+            else:
+                self.table.pop(cont)
+                for i in delimiters:
+                    self.table.insert(cont, i.group())
+                    cont += 1
+
+
+    def readCode(self, text):
+        self.separate_delimits(text)
+        print(self.get_table())
+        self.separate_whitespace()
+        print(self.get_table())
+    
+    def get_table(self):
+        return self.table
+
+lex = LexicalAnalyser()
+ex = "void main() {\nint a, b, c;\n c = a + b; }"
+lex.readCode(ex)
