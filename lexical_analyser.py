8a7f85c6ca95819d49cde4bc6f335658976c7dd2
luigim1998 <sluigimuller@gmail.com>
2020-03-26 21:07:18 -0400

    analyse token, separate operators and whitespaces

--- lexical_analyser.py
+++ lexical_analyser.py
@@ -68,40 +68,102 @@ class LexicalAnalyser:
             return True
         return False
 
-    def separate_delimits(self, text):
+    def separate_text_delimits(self, text):
+        cont = 0
         m = re.compile(self.DELIMIT)
         delimiters = list(m.finditer(text))
-        cont = 0
+        out = []
 
         for i in delimiters:
             if(cont != i.start()): #existe algo antes do delimitador
-                self.table.append(text[cont:i.start()])
-            self.table.append(text[i.start()]) #adiciona o delimitador
+                out.append(text[cont:i.start()])
+            out.append(text[i.start()]) #adiciona o delimitador
             cont = i.start()+1
         
         if(cont != len(text)): #existe texto após o último delimitador
-            self.table.append(text[cont:])
+            out.append(text[cont:])
+        
+        return out
 
-    def separate_whitespace(self):
-        cont = 0
-        p = re.compile("[\S]+")
-        while(cont < len(self.table)):
-            delimiters = list(p.finditer(self.table[cont]))
-            if(len(delimiters) == 0): # não há texto
-                self.table.pop(cont)
-                cont += 1
-                # print("Cont: {}, Texto: {}, lista: {}".format(cont, self.table[cont], self.table))
-            else:
-                self.table.pop(cont)
-                for i in delimiters:
-                    self.table.insert(cont, i.group())
-                    cont += 1
+    def separate_text_whitespace(self, text):
+        m = re.compile("[\S]+")
+        # delimiters = list(m.finditer(text))
+        # out = []
+        # for i in delimiters:
+        #     out.append(i.group())
+        # return out
+        return m.findall(text)
 
+    def separate_whitespace(self, table):
+        out = []
+
+        for i in table:
+            words = self.separate_text_whitespace(i)
+            if(len(words) != 0): # não há texto
+                out.extend(words)
+        
+        return out
+
+    def separate_text_operators(self, text):
+        out = [text]
+
+        for i in ["relational", "logical", "assignment", "arithmetic", "inc_dec"]:
+            for j in self.operators[i]:
+                cont = 0
+                while(cont < len(out)):
+                    if(out[cont].find(j) != -1):
+                        out = out[:cont] + list(out[cont].partition(j)) + out[cont+1:]
+                        cont += 2
+                    else:
+                        cont += 1
+        return out
+    
+    def separate_operators(self, table):
+        out = []
+
+        for i in table:
+            words = self.separate_text_operators(i)
+            if(len(words) != 0): # não há texto
+                out.extend(words)
+        
+        return out
+
+    def analyse_token(self, text):
+        label = ''
+        if(self.isKeyword(text)):
+            label = 'KEYWORD'
+        elif(self.isIdentifier(text)):
+            label = 'IDENTIFIER'
+        elif(self.isString(text)):
+            label = 'STRING'
+        elif(self.isDelimit(text)):
+            label = 'DELIMITER'
+        elif(self.isOperator(text)):
+            label = 'OPERATOR'
+        elif(self.isInteger(text)):
+            label = 'NUMBER'
+        elif(self.isFloat(text)):
+            label = 'NUMBER'
+        else:
+            label = 'INVALID'
+        return label
+
+    def classify_tokens(self, table):
+        out = []
+
+        for i in table:
+            out.append([i, self.analyse_token(i)])
+        
+        return out
 
     def readCode(self, text):
-        self.separate_delimits(text)
+        self.table = self.separate_text_delimits(text)
         print(self.get_table())
-        self.separate_whitespace()
+        self.table = self.separate_operators(self.table)
+        print(self.get_table())
+        self.table = self.separate_whitespace(self.table)
+        print(self.get_table())
+        self.table = self.classify_tokens(self.table)
         print(self.get_table())
     
     def get_table(self):
@@ -109,4 +171,5 @@ class LexicalAnalyser:
 
 lex = LexicalAnalyser()
 ex = "void main() {\nint a, b, c;\n c = a + b; }"
-lex.readCode(ex)
+ex2 = "void main() {\nint a, b, c;\n c = a + b;\n a=c-b;}"
+lex.readCode(ex2)
